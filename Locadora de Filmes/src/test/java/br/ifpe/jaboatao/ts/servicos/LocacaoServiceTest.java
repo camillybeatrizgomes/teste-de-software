2b868321711094af9b3b2f6687f42126a4ab8c6a
Camilly Beatriz <camillybeatriz2017@gmail.com>
2023-07-18 23:00:52 -0300

    implementando builder

--- Locadora de Filmes/src/test/java/br/ifpe/jaboatao/ts/servicos/LocacaoServiceTest.java
+++ Locadora de Filmes/src/test/java/br/ifpe/jaboatao/ts/servicos/LocacaoServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import br.ifpe.jaboatao.ts.builder.FilmesBuilder;
 import br.ifpe.jaboatao.ts.builder.UsuarioBuilder;
 import br.ifpe.jaboatao.ts.entidades.Filme;
 import br.ifpe.jaboatao.ts.entidades.Locacao;
@@ -23,7 +24,7 @@ public class LocacaoServiceTest {
 		// Cenário
 		LocacaoService service = new LocacaoService();
 		Usuario usuario = UsuarioBuilder.umUsuario().agora();
-		List<Filme> filmes = Arrays.asList(new Filme("Filme 01", 10, 11.0));
+		List<Filme> filmes = Arrays.asList(FilmesBuilder.umFilme().agora());
 
 		// Ação
 		Locacao locacao = service.alugarFilme(filmes, usuario);
@@ -38,8 +39,8 @@ public class LocacaoServiceTest {
 	public void excecao01() {
 		// Cenário
 		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuário 01");
-		List<Filme> filmes = Arrays.asList(new Filme("Filme 01", 0, 11.0));
+		Usuario usuario = UsuarioBuilder.umUsuario().outroNome("Usuario 02").agora();
+		List<Filme> filmes = Arrays.asList(FilmesBuilder.umFilme().semEstoque().agora());
 		// Ação
 		try {
 			Locacao locacao = service.alugarFilme(filmes, usuario);
@@ -56,8 +57,8 @@ public class LocacaoServiceTest {
 	public void excecao02() {
 		// Cenário
 		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuário 01");
-		List<Filme> filmes = Arrays.asList(new Filme("Filme 01", 0, 11.0));
+		Usuario usuario = UsuarioBuilder.umUsuario().agora();
+		List<Filme> filmes = Arrays.asList(FilmesBuilder.umFilme().semEstoque().agora());
 
 		// Ação
 		FilmesException filmesException = Assertions.assertThrows(FilmesException.class, () -> {
@@ -72,7 +73,7 @@ public class LocacaoServiceTest {
 	public void excecao03() {
 		// Cenário
 		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuario 01");
+		Usuario usuario = UsuarioBuilder.umUsuario().agora();
 
 		// Ação
 		try {
@@ -88,7 +89,7 @@ public class LocacaoServiceTest {
 	public void excecao04() {
 		// Cenário
 		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuario 01");
+		Usuario usuario = UsuarioBuilder.umUsuario().agora();
 
 		// Ação
 		FilmesException filmesException = Assertions.assertThrows(FilmesException.class, () -> {
@@ -103,8 +104,8 @@ public class LocacaoServiceTest {
 	public void excecao05() {
 		// Cenário
 		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("");
-		List<Filme> filmes = Arrays.asList(new Filme("Filme 01", 0, 11.0));
+		Usuario usuario = UsuarioBuilder.usuarioVazio().agora();
+		List<Filme> filmes = Arrays.asList(FilmesBuilder.umFilme().agora());
 
 		// Ação
 		try {
@@ -120,8 +121,8 @@ public class LocacaoServiceTest {
 	public void excecao06() {
 		// Cenário
 		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("");
-		List<Filme> filmes = Arrays.asList(new Filme("Filme 01", 0, 11.0));
+		Usuario usuario = UsuarioBuilder.usuarioVazio().agora();
+		List<Filme> filmes = Arrays.asList(FilmesBuilder.umFilme().agora());
 
 		// Ação
 		FilmesException filmesException = Assertions.assertThrows(FilmesException.class, () -> {
